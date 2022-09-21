172849b89e2bdae779db267c21750cefc55289b6
Moringa School <joeleyian6@gmail.com>
2022-09-21 11:40:44 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+var employee = {}
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    var newEmployee = employee.assig({}, ...employee, {[key]: value});
+    return newEmployee;
+}
+
+function destructivelyupdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee
+}
+function deleteFromEmployeeByKey(employee, key){
+    const NEW_OBJ = Object.assign({}, employee);
+    delete employee[key];
+    return employee;
+}
+
+function deleteEmployee(employee) {
+    const NEW_OBJ = Object.assign({}, employee);
+    delete NEW_OBJ;
+    return false;
+}
+
+function destructivelyDeleteEmployee(employee) {
+    delete employee;
+    return false;
+}
